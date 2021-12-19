a2a1d1856df11eafb0fab9a00b129ba02d57325b
Undefinedefity <11711808@mail.sustech.edu.cn>
2021-12-20 04:47:29 +0800

    wuyuzi

--- results/modify_ldso.py
+++ results/modify_ldso.py
@@ -1,7 +1,9 @@
-file = open('kitti_ldso_00.txt', 'r+', encoding='utf-8')
-file_cp = open('kitti_ldso_00_copy.txt', 'r+', encoding='utf-8')
-for i in file_cp:
-    idx = i.find(' ')
-    file.write(i[idx:])
-file.close()
-file_cp.close()
+j = [0,1,2]
+for k in j:
+    file = open('kitti_ldso_0'+str(k)+'.txt', 'r+', encoding='utf-8')
+    file_cp = open('kitti_ldso_0'+str(k)+'_copy.txt', 'r+', encoding='utf-8')
+    for i in file_cp:
+        idx = i.find(' ')
+        file.write(i[idx+1:])
+    file.close()
+    file_cp.close()
