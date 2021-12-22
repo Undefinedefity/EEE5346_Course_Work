48cd5f03edbb2bdf0a9e84799bc1a3be012f2ba4
Undefinedefity <11711808@mail.sustech.edu.cn>
2021-12-22 15:37:56 +0800

    updated graph

--- results/kitti/cal_map.sh
+++ results/kitti/cal_map.sh
@@ -1,17 +1,16 @@
 #! /bin/bash
 
-# folder_gt="./gt"
-# folder_corrected="./corrected"
+folder_gt="./gt"
+folder_corrected="./corrected"
 
-# for sqe in 00 01 02 05 07
-# do
-#     dso="kiiti_dso_${sqe}.txt"
-#     ldso="kiiti_ldso_${sqe}.txt"
-#     orbslam2="kiiti_orbslam2_${sqe}.txt"
-#     gt="kiiti_gt_${sqe}.txt"
-#     evo_traj tum ${folder_corrected}/${dso} ${folder_corrected}/${ldso} ${folder_corrected}/$orbslam2 --ref=${folder_gt}/$gt -p -s -a
-# done
+for sqe in 01 02 10 20 25
+do
+    dso="tum_dso_${sqe}.txt"
+    ldso="tum_ldso_${sqe}.txt"
+    gt="tum_gt_${sqe}.txt"
+    evo_traj tum ${folder_corrected}/${dso} ${folder_corrected}/${ldso} --ref=${folder_gt}/$gt -p -s -a
+done
 
-evo_traj tum ./corrected/kitti_dso_02.txt ./corrected/kitti_ldso_02.txt ./corrected/kitti_orbslam2_02.txt --ref=./gt/kitti_gt_02.txt -p -s -a
-evo_traj tum ./corrected/kitti_dso_05.txt ./corrected/kitti_ldso_05.txt ./corrected/kitti_orbslam2_05.txt --ref=./gt/kitti_gt_05.txt -p -s -a
-evo_traj tum ./corrected/kitti_dso_07.txt ./corrected/kitti_ldso_07.txt ./corrected/kitti_orbslam2_07.txt --ref=./gt/kitti_gt_07.txt -p -s -a
+# evo_traj tum ./corrected/kitti_dso_02.txt ./corrected/kitti_ldso_02.txt ./corrected/kitti_orbslam2_02.txt --ref=./gt/kitti_gt_02.txt -p -s -a
+# evo_traj tum ./corrected/kitti_dso_05.txt ./corrected/kitti_ldso_05.txt ./corrected/kitti_orbslam2_05.txt --ref=./gt/kitti_gt_05.txt -p -s -a
+# evo_traj tum ./corrected/kitti_dso_07.txt ./corrected/kitti_ldso_07.txt ./corrected/kitti_orbslam2_07.txt --ref=./gt/kitti_gt_07.txt -p -s -a
